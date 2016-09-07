8fd8d69d68ff17648a5d10bde3f8f0d650d847c4
Brian Boltiansky <BBoltiansky18@windwardschool.org>
2016-09-07 09:09:35 -0700

    Team project
    Working together to create starfigures

---
+++ src/StarFigures.java
@@ -0,0 +1,21 @@
+
+public class StarFigures {
+	public static void main(String[] args){
+		starSeg();
+		starSeg();
+		lineSeg();
+		starSeg();
+	}
+	public static void starSeg(){
+		System.out.println("\n*****\n");
+		System.out.println("*****\n");
+		System.out.println(" * * \n");
+		System.out.println("  *\n");
+		System.out.println(" * *\n");
+	}
+	public static void lineSeg(){
+		System.out.println("*****\n");
+		System.out.println("*****\n\n");
+		System.out.println("  *\n\n  *\n\n  *");
+	}
+}
